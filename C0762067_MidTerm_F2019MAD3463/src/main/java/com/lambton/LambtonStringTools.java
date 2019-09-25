27dd4be0e2143d4235a7b8016321a4a5e5aba9da
ritik jagpal <ritikjagpal@gmail.com>
2019-09-25 18:36:26 -0400

    commit

--- C0762067_MidTerm_F2019MAD3463/src/main/java/com/lambton/LambtonStringTools.java
+++ C0762067_MidTerm_F2019MAD3463/src/main/java/com/lambton/LambtonStringTools.java
@@ -1,4 +1,16 @@
 package com.lambton;
 
 public class LambtonStringTools {
+    public void reverse(String s)
+    {
+        String b = "";
+        String a=s;
+        for (int i = a.length()-1; i >= 0; i--)
+        {
+            b= b + a.charAt(i);
+        }
+        System.out.println("Reverse of String  :"+b);
+
+    }
 }
+
---
+++ C0762067_MidTerm_F2019MAD3463/src/main/java/com/lambton/Main.java
@@ -0,0 +1,9 @@
+package com.lambton;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        LambtonStringTools display=new LambtonStringTools();
+        display.reverse("Lambton");
+    }
+}
